65eabb707e238ef8e374285dde540342d86a73fe
jennyjean <jennyjean8675309@yahoo.com>
2018-10-26 13:33:32 -0400

    added to rspec file

--- bin/run.rb
+++ bin/run.rb
@@ -2,25 +2,3 @@ require_relative '../config/environment'
 
 game = TriviaGame.new
 game.play
-
-
-
-# #Begins Round 1 (easy)
-# difficulty = "easy"
-# puts "Hi #{user_name}, choose a category:"
-# Category.output_categories
-# selected_category = gets.chomp
-#
-# q1 = Question.give_user_question(selected_category, "easy")
-#
-# user_answer = gets.chomp
-# uq1 = UserQuestion.create(user: user1, question: q1)
-#
-# uq1.tells_user_if_correct(user_answer)
-# uq1.keep_score(user_answer)
-#
-# puts "Your score is #{user1.score}"
-#
-#
-#
-# #Begins Round 2
--- spec/trivia_game_spec.rb
+++ spec/trivia_game_spec.rb
@@ -1,4 +1,5 @@
-# require_relative 'spec_helper'
+require 'pry'
+
 require_relative '../lib/question.rb'
 require_relative '../lib/category.rb'
 require_relative '../lib/choice.rb'
@@ -12,28 +13,25 @@ describe Question do
   cat1 = Category.new(name: "Movies")
   cat2 = Category.new(name: "Science")
 
+  q1 = Question.new(question: "How many Jaws movies are there?", category: cat1, difficulty: "easy")
+  q2 = Question.new(question: "What is the name of Annie's dog in Annie?", category: cat1, difficulty: "hard")
+  q3 = Question.all[0]
+
+  choice1 = Choice.new(name: "a gazillion", question: q1, correct: true)
+  choice2 = Choice.new(name: "not that many", question: q1, correct: false)
+  q1.choices << choice1
+  q1.choices << choice2
+
   it "belongs to a category" do
-    q1 = Question.new(question: "How many Jaws movies are there?", category: cat1, difficulty: "easy")
-    expect(Question.find_by(question: "How many Jaws movies are there?").category.to eq(cat1))
+    expect(q1.category).to eq(cat1)
   end
 
   it "has a difficulty level" do
-    q2 = Question.create(question: "What is the name of Annie's dog in Annie?", category: cat1, difficulty: "hard")
-    expect(Question.find_by(question: "What is the name of Annie's dog in Annie?").difficulty.to eq("hard"))
+    expect(q2.difficulty).to eq("hard")
   end
 
-end
-
-
-#Category class tests
-describe Category do
-
-  cat3 = Category.new(name: "Books")
-  q1 = Question.new(question: "Who is the author of To Kill a Mockingbird?", category: cat3, difficulty: "medium")
-  q2 = Question.new(question: "How many books are in the Harry Potter series?", category: cat3, difficulty: "easy")
-
-  it "has many questions" do
-    expect(cat3.questions.count).to eq(2)
+  it "can find its correct answer" do
+    expect(q1.correct_answer).to eq(choice1)
   end
 
 end
@@ -52,7 +50,7 @@ describe Choice do
   end
 
   it "returns a boolean for whether or not it is the correct choice" do
-    expect(choice1.correct).to eq("false")
+    expect(choice1.correct).to eq(false)
   end
 
 end
@@ -62,14 +60,26 @@ cat1 = Category.new(name: "Movies")
 cat2 = Category.new(name: "Science")
 
 q1 = Question.new(question: "What is the lightest element?", category: cat2, difficulty: "easy")
-q2 = Question.new(name: "What is the name of Jack Nicholson's character in The Shining?", category: cat2, difficulty: "easy")
+q2 = Question.new(question: "What is the name of Jack Nicholson's character in The Shining?", category: cat2, difficulty: "easy")
+
 
 mike = User.new(name: "Mike")
 jenny = User.new(name: "Jenny")
 
+#User class tests
+describe User do
+
+  it "instantiates a user with a score of 0" do
+    expect(mike.score).to eq(0)
+  end
+
+end
+
 
 #UserQuestion class tests
 describe UserQuestion do
+
+ uq3 = UserQuestion.all[0]
 
  it "belongs to a question" do
   uq1 = UserQuestion.new(question: q1, user: mike)
@@ -79,6 +89,15 @@ describe UserQuestion do
  it "belongs to a user" do
    uq2 = UserQuestion.new(question: q2, user: jenny)
    expect(uq2.user).to eq(jenny)
+ end
+
+ it "validates the user input (answer) for a particular question and returns true if the answer is correct and false if the answer is incorrect" do
+   correct = Choice.all.find { |choice| choice.name == "George Washington" }
+   choices = uq3.question.connect_letter_to_choice
+   random = uq3.question.randomized_choices
+   user_input = random.select { |letter, answer| answer.name == "George Washington"}
+   user_input_final = user_input.keys[0]
+   expect(uq3.validate_user_input(user_input_final)).to eq(true)
  end
 
 end
